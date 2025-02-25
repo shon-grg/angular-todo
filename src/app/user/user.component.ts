e08263f16b5050965dd0211a31960b21a97ba628
Shon George <shongeorge2000@gmail.com>
2025-02-25 09:52:37 +0530

    second Commit

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, input } from '@angular/core';
+import {
+  Component,
+  computed,
+  EventEmitter,
+  Input,
+  input,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,14 +14,16 @@ import { Component, Input, input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // @Input({ required: true }) avatar!: string;
-  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
-  avatar = input.required<string>();
-  name = input.required<string>();
+  @Output() select = new EventEmitter();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
-  onSelectUser() {}
+  onSelectUser() {
+    this.select.emit();
+  }
 }
